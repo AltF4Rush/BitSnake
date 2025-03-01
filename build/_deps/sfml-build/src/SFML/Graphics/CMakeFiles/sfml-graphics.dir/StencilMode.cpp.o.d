46fcf840535a469c4ae65907eccea11480284fec
darkkboy0013@gmail.com <darkkboy0013@gmail.com>
2025-03-01 11:40:49 +0000

    Ready to start

---
+++ build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o.d
@@ -0,0 +1,6 @@
+_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o: \
+ /home/kaido/Projects/BitSnake/build/_deps/sfml-src/src/SFML/Graphics/StencilMode.cpp \
+ /usr/include/stdc-predef.h \
+ /home/kaido/Projects/BitSnake/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
+ /home/kaido/Projects/BitSnake/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
+ /home/kaido/Projects/BitSnake/build/_deps/sfml-src/include/SFML/Config.hpp
